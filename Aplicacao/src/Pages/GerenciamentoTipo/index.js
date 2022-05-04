eb7055e248059e5477d42133354ee3d75bfd7fda
Arley Souto <32252053+mercurio236@users.noreply.github.com>
2022-05-04 13:26:43 -0300

    Feito ajustes na tela de Gerenciamento de Tipo
    Foi feito as principais requisições

--- Aplicacao/src/Pages/GerenciamentoTipo/index.js
+++ Aplicacao/src/Pages/GerenciamentoTipo/index.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../Components/Connections/api';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import {
     Container,
-    Typography
+    Typography,
+    TextField
 } from '@material-ui/core';
 
 
@@ -14,44 +15,53 @@ export default function GerenciamentoTipo() {
     const [putTypes, setPutTypes] = useState('');
     const [deleteTypes, setDeleteTypes] = useState('');
     const [editField, setEditField] = useState(false);
-
-    const dispatch =  useDispatch();
+    const [nameType, setNmaeType] = useState('');
     const userL = useSelector((state) => state.userState.userLogado)
     var user = userL ? userL.uid : null
 
 
     useEffect(() => {
         getTypes();
-    }, [])
+    }, [getType])
 
-    async function deleteType(tipo){
-        console.log(tipo)
-        await api.get(`types?login=${user}&type=${tipo}`)
-        .then((res) =>{
-            /* let newList = getType.filter(item =>  item.data !== tipo)
-            setGetType(newList) */
-            console.log('sucesso')
-            console.log(res.data)
-        })
-        .catch((err) =>{
-            console.log(err)
-        })
+    async function deleteType(tipo) {
+        //console.log(tipo)
+        await api.delete(`types?login=${user}&type=${tipo}`)
+            .then((res) => {
+                /* let newList = getType.filter(item =>  item.data !== tipo)
+                setGetType(newList) */
+                console.log('sucesso')
+                console.log(res.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
     }
 
-    async function putType(dev){
-        console.log(dev)
-        await api.put(`types?login=${user}&type=${putTypes}`)
-        .then((res) =>{
-            if(res.data == ''){
-                console.log('Erro ao editar')
-            }
-            else{
-                console.log('Editado com sucesso')
-            }
-        })
-        .catch((err) =>{
-            console.log(err)
-        })
+    async function putType(type) {
+        const data = {
+            name: nameType
+        }
+        await api.put(`types?login=${user}&type=${type}`, data)
+            .then((res) => {
+                if (res.data === '') {
+                    console.log('Erro ao editar')
+                }
+                else {
+                    console.log('Editado com sucesso')
+                    setEditField(false)
+                }
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
+    async function handleEditField(tipo) {
+        console.log(tipo)
+        setEditField(true)
+        setNmaeType(tipo)
+        setPutTypes(tipo)
     }
 
     async function getTypes() {
@@ -64,15 +74,7 @@ export default function GerenciamentoTipo() {
             })
     }
 
-    const typesMap = (tipo, index) => {
-        return (
-            <div key={index}>
-                <Typography>{tipo}</Typography>
-                <button onClick={() => putType(tipo)} >Atualizar</button>
-                <button onClick={() => deleteType(tipo)}>Delete</button>
-            </div>
-        )
-    }
+
 
     return (
         <Container>
@@ -80,10 +82,36 @@ export default function GerenciamentoTipo() {
                 <Typography>Iniciando gerenciamento de Tipo</Typography>
             </div>
             <div>
-
                 {
-                editField && 
-                getType.map(typesMap)}
+                    getType.map((ty, index) => (
+                        <div>
+                            <Typography>
+                                Nome do Tipo:
+                            </Typography>
+                            {
+                                editField && putTypes == ty ?
+                                    <div >
+                                        <TextField
+                                            key={ty}
+                                            label="Editar nome do Tipo"
+                                            value={nameType}
+                                            onChange={(e) => setNmaeType(e.target.value)}
+                                        />
+                                        <button onClick={() => putType(ty)}>Salvar</button>
+                                        <button onClick={() => setEditField(!editField)}>Cancelar</button>
+                                    </div>
+                                    :
+                                    <div key={index}>
+                                        <Typography>{ty}</Typography>
+                                        <button onClick={() => handleEditField(ty)} >Editar</button>
+                                        <button onClick={() => deleteType(ty)}>Delete</button>
+                                    </div>
+                            }
+
+                        </div>
+                    ))
+
+                }
             </div>
         </Container>
     )
