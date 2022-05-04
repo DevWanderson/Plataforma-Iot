25bc569c993edeb0d0ac46aefbaeb3044d460c5a
Arley Souto <arley.souto@hotmail.com>
2022-05-04 01:13:03 -0300

    Atualização

--- Aplicacao/src/Pages/GerenciamentoTipo/index.js
+++ Aplicacao/src/Pages/GerenciamentoTipo/index.js
@@ -1,2 +1,11 @@
 import React from 'react';
+import { Container, Typography } from '@material-ui/core';
 
+
+export default function GerenciamentoTipo(){
+    return(
+        <Container>
+            <Typography>Iniciando gerenciamento de Tipo</Typography>
+        </Container>
+    )
+}
